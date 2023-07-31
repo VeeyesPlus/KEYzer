ee12c64735883c44c6b137df858707771785b863
VeeyesPlus <129393961+VeeyesPlus@users.noreply.github.com>
2023-07-31 12:00:40 +0530

    loader.py

--- loader.py
+++ loader.py
@@ -1,6 +1,6 @@
 import spacy
 
 def load_spacy_model():
-    model_path = "C:/Users/vpadimiti/AppData/Local/Programs/Python/Python39\spacy_models/en_core_web_sm"
+    model_path = "path to spacy_models/en_core_web_sm"
     nlp = spacy.load(model_path)
     return nlp
